d400ef353ca08013ed3d1cadd6ebfdc9060a425b
AlishiaWilcher <stown400@gmail.com>
2017-10-12 16:43:32 -0500

    created functions_js and functions_Html files.

---
+++ js/external.js
@@ -0,0 +1,7 @@
+"use strict";
+
+console.log("Hello from external JavaScript");
+alert("Welcome to my Website!");
+
+var userInput = prompt("What's your favorite color?");
+alert( "Great, " + userInput + " is my favorite color too!");
