a73da2894a6c0cb102bb849230bdf68e1cdf948e
AlishiaWilcher <stown400@gmail.com>
2017-10-19 10:31:34 -0500

    created and worked on file

---
+++ js/for_loops.js
@@ -0,0 +1,96 @@
+"use strict";
+
+//
+// function showMultiplicationTable(number){
+//
+//     for(var i = 1; i <= 10; i+=1){
+//
+//         console.log(number + " x " + i + "=" + (number * i));
+//     }
+// }
+//
+// showMultiplicationTable(5);
+
+
+
+
+// for (var i = 100; i > 0;) {
+//     console.log( + i);
+//     i = i - 5
+//
+// }
+
+
+// Use a for loop and the code from the previous lessons to generate 10 random numbers between 20 and 200
+// and output to the console whether each number is odd or even. For example:
+
+
+
+for (var i = 1; i <= 10; i++) {
+    var randomNumber = Math.floor(Math.random() * 200) + 20;
+    if(randomNumber % 2 == 0) {
+    console.log(randomNumber + "is even");
+    }else {
+
+
+    console.log(randomNumber + "is odd");
+    }
+
+
+}
+
+
+
+
+
+
+//         var text = "";
+// var i = 0;
+//
+// // The first for loop
+// for  (i < 10; i++;) {
+//     text +=  i;
+//
+//     // The second for loop
+//     for (j = 10; j < 10; j++) {
+//         if (j === 12) {
+//         }
+//     }
+// }
+
+
+//
+//
+//
+// var half = (size/2)+1;
+// var a = 1;
+// var b = a;
+// var c = size;
+// var printf = "";
+//
+//
+// for (a <=  half ; a++) {
+//     for (b<half;b++) {
+//         printf(" ");
+//     }
+//     for (c -2* a; c <=  half; c++)
+//     {
+//         printf("*");
+//     }
+//     printf("\n");
+// }
+// for (a = 1; a < half;a++)
+// {
+//     for (b = a; b>0;b--)
+//     {
+//         printf(" ");
+//     }
+//     for (c-2*a; c > 0 ;c--)
+//     {
+//         printf("*");
+//     }
+//     printf("\n");
+// }
+//
+//
+// return 0;
