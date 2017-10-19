8a172d0d262db4949a3b1c79608e95f67ea88ae2
AlishiaWilcher <stown400@gmail.com>
2017-10-19 10:29:27 -0500

    made js and html file.

--- js/while.js
+++ js/while.js
@@ -2,16 +2,32 @@
 
 
 
-var i = 2;
-while (i < 65537) {
-    console.log (i);
-    i *= 2;
-}
+// var i = 2;
+//
+// while (i <= 65536) {
+//     console.log (i);
+//     i *= 2;
+// }
 
 
 // This is how you get a random number between 50 and 100
-var allCones = Math.floor(Math.random() * 50) + 50;
+
 // // This expression will generate a random number between 1 and 5
-Math.floor(Math.random() * 5) + 1;
 
+var allCones = Math.floor(Math.random() * 50) + 50;
 
+console.log("Starting inventory is " + allCones);
+
+do {
+    var conesOrdered = Math.floor(Math.random() * 5) + 1;
+
+    if (conesOrdered <= allCones) {
+        console.log("the customer ordered " + conesOrdered);
+        allCones = allCones - conesOrdered;
+        console.log("I sold " + conesOrdered);
+        console.log("My inventory is " + allCones);
+    } else {
+        console.log("next customer please");
+    }
+} while(allCones > 0);
+console.log("cones all sold out.");
