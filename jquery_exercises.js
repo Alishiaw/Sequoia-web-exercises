389be432cf0b990070b4c4acc93d9d09581a1ce3
AlishiaWilcher <stown400@gmail.com>
2017-10-30 16:26:01 -0500

    started jquery

---
+++ jquery_exercises.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+$('h1').click(function(){
+
+    $(this).css("background-color", "#FF0")
+});
+
+$("p").dblclick(function(){
+    $(this).css("font-size", "18px")
+});
+
+$("li").hover(
+    function () {
+        $(this).css("color", "#F00")
+    },
+    function () {
+        $(this).css("color", "#000")
+    }
+)
+
+
+});
+
+"use strict";
